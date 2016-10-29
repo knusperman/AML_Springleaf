cd9ebd6202fd718600de546b35ae2858a2bd1029
Markus <m.heuchert@uni-muenster.de>
2016-10-29 19:26:58 +0200

    exploratory analysis

--- setup.R
+++ setup.R
@@ -1,3 +1,8 @@
 #temp path for markus.. upload data to git when fast wifi available
-train_1k <- read.csv("/Volumes/Nifty/springleaf_train.csv",nrows = 1000)
+train <- read.csv("/Volumes/Nifty/springleaf_train.csv",n_max = 20000)
 #test
+y = train$target
+# remove the id and target
+train = subset(train, select=-c(ID, target))
+# get the rowcount
+
