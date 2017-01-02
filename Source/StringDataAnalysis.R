bf5059cd0721a6e13664f02866458fbdc87f4fad
Marcus Cramer <marcus_cramer@t-online.de>
2017-01-02 20:08:17 +0100

    string data finalized

--- Source/StringDataAnalysis.R
+++ Source/StringDataAnalysis.R
@@ -85,9 +85,12 @@ ggplot(data = plotData, aes(x = Var1, y = Freq)) +
   theme(plot.margin = unit(c(1,2,1,1), "cm"))
 dev.off()
 
-# check for the other strings by comparing the maximum of the response rate vs. the minimum 
+# check for all strings by comparing the maximum of the response rate vs. the minimum 
 # (for the 10 highest occurrences)
-stringCol = seq(1, 21, by = 1)
+# reason for this: check if there are any significant differences regarding the highest occurrences and their impact on the target
+# the higher the variance, the more impact on the target value
+# this is just done as an overview, strings with a low NA percentage and low number of unique values are used anyway
+stringCol = seq(1, 17, by = 1)
 result = numeric(0)
 naPercentage = numeric(0)
 j = 1
@@ -108,4 +111,66 @@ for (i in stringCol) {
 }
 result = cbind(VarianceResponse = result, UniqueValues = uniqueValues, naPercentage = naPercentage)
 
+# how to process the data:
+# remove cities (3), job descriptions (14, 16) (-> overfitting)
+# remove 4 because of 99.99% NAs
+# remove 
+# handle the rest as factors
+# bundle states manually (the lower tail) because randomForest can only handle up to 53 factor levels
+relevantData = stringData[,-c(3, 14, 16, 4)]
+# handle states
+plotData = as.data.frame(sort(table(relevantData[,3]), decreasing = TRUE))
+plotData = cbind(rownames(plotData), plotData)
+plotData[,1] = factor(plotData[,1], levels = unique(plotData[,1]))
+colnames(plotData) = c("id", "occurrences")
+png("fig/states1.png", height = 800, width = 800)
+ggplot(data = plotData, aes(x = id, y = occurrences)) + 
+  geom_bar(stat = "identity") + xlab("States") + ylab("Number of occurrences") + 
+  theme_bw() +
+  theme(axis.text = element_text(size = 40, colour = "black", angle = 45, hjust = 1), 
+        axis.title = element_text(size = 40, colour = "black")) +
+  theme(plot.margin = unit(c(1,2,1,1), "cm")) +
+  theme(axis.text.x=element_blank())
+dev.off()
 
+plotData = as.data.frame(sort(table(relevantData[,4]), decreasing = TRUE))
+plotData = cbind(rownames(plotData), plotData)
+plotData[,1] = factor(plotData[,1], levels = unique(plotData[,1]))
+colnames(plotData) = c("id", "occurrences")
+png("fig/states2.png", height = 800, width = 800)
+ggplot(data = plotData, aes(x = id, y = occurrences)) + 
+  geom_bar(stat = "identity") + xlab("States") + ylab("Number of occurrences") + 
+  theme_bw() +
+  theme(axis.text = element_text(size = 40, colour = "black", angle = 45, hjust = 1), 
+        axis.title = element_text(size = 40, colour = "black")) +
+  theme(plot.margin = unit(c(1,2,1,1), "cm")) +
+  theme(axis.text.x=element_blank())
+dev.off()
+
+sum()
+# very long tail -> bundle all states that have less than 1000 occurrences
+# since we work with samples for training the predictors initially, this is required to ensure some sort of stable behavior
+stateData1 = as.character(relevantData[,3])
+stateData2 = as.character(relevantData[,4])
+filter1 = as.data.frame(which(table(stateData1) < 1000))
+filter2 = as.data.frame(which(table(stateData2) < 1000))
+rownames(filter1) %in% rownames(filter2) # the first filter almost is a subset of the second filter
+# set the value of everything that matches these filters to "other"
+stateData1[stateData1 %in% rownames(filter1)] = "other"
+stateData2[stateData2 %in% rownames(filter2)] = "other"
+
+# merge back with relevantData
+relevantData[,3] = stateData1
+relevantData[,4] = stateData2
+
+# for all: convert NAs into another factor level
+for (i in 1:ncol(relevantData)) {
+  relevantData[,i] = as.character(relevantData[,i])
+  relevantData[is.na(relevantData[,i]),i] = "na"
+}
+# factorize
+for (i in 1:ncol(relevantData)) {
+  relevantData[,i] = factor(relevantData[,i], levels = unique(relevantData[,i]))
+}
+
+saveRDS(relevantData, "data/stringData_FINAL.rds")
