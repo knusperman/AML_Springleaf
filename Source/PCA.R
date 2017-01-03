54b6a79032a69838423999b6c5e2a5ea7a90b12f
Marcus Cramer <marcus_cramer@t-online.de>
2017-01-04 00:35:10 +0100

    asdf

--- Source/PCA.R
+++ Source/PCA.R
@@ -8,8 +8,13 @@ n7 <- readRDS("data/numeric imputations/imp110-130k.rds") #11001 - 130000
 n8 <- readRDS("data/numeric imputations/imp130k-end.rds") 
 train_imputed <-rbind(n1,n2,n3,n4,n5,n6,n7,n8)
 
-#perform imputation ...
-
-which(sapply(n3, function(x)sum(is.na(x)))>0)
-df_imputed[rownames(n3),] ->n3i
-saveRDS(n3i, "data/numeric imputations/imp40-50k.rds")
+pcaRes = princomp(train_imputed[complete.cases(train_imputed),], cor = TRUE)
+plotData = as.data.frame(cbind(id = seq_along(pcaRes$sdev), sdev = pcaRes$sdev))
+png("fig/pcaSdev.png", height = 800, width = 800)
+ggplot(data = plotData, aes(x = id, y = sdev)) + 
+  geom_line() + xlab("Attributes") + ylab("Standard deviation") +
+  theme_bw() +
+  theme(axis.text = element_text(size = 40, colour = "black", angle = 45, hjust = 1), 
+        axis.title = element_text(size = 40, colour = "black")) +
+  theme(plot.margin = unit(c(1,2,1,1), "cm"))
+dev.off()
