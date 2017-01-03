76cda4116861b7a3965c885c93aa384dc8e95440
markusheuchert <m.heuchert@uni-muenster.de>
2017-01-04 00:26:37 +0100

    last imp

--- data/numeric imputations/imputation merge.R
+++ data/numeric imputations/imputation merge.R
@@ -4,13 +4,15 @@ n3 <- readRDS("data/numeric imputations/imp30-40k.rds") #30001-40000
 n4 <- readRDS("data/numeric imputations/imp40-50k.rds") # 40001 - 50000
 n5 <- readRDS("data/numeric imputations/imp50-70k.rds") #50001 - 70000
 n6 <- readRDS("data/numeric imputations/imp70-90k.rds") #700001 - 90000
-n7 <- readRDS("data/numeric imputations/imp110-130k.rds") #11001 - 130000
-n8 <- readRDS("data/numeric imputations/imp130k-end.rds") 
-train_imputed <-rbind(n1,n2,n3,n4,n5,n6,n7,n8)
-
-#perform imputation ...
-
-which(sapply(n3, function(x)sum(is.na(x)))>0)
+n7 <- readRDS("data/numeric imputations/imp110-130k.rds") #110001 - 130000
+n8 <- readRDS("data/numeric imputations/imp130k+.rds") #130001 - 145231
+n8a <- readRDS("data/numeric imputations/xxximputed130k-end.rds")
+train_imputed <-rbind(n1,n2,n3,n4,n5,n6,n7,n8a)
+remove(n1,n2,n3,n4,n5,n6,n7,n8)
+df<- train_imputed
+which(sapply(n8b, function(x)sum(is.na(x)))>0)
 df_imputed[rownames(n3),] ->n3i
 saveRDS(n3i, "data/numeric imputations/imp40-50k.rds")
 
+set.seed(1234)
+s <- sample(1:145231, 145231)
