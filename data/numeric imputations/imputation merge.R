7439a8f47a1c03f4f0ccfe52f54f525867299f6b
markusheuchert <m.heuchert@uni-muenster.de>
2017-01-04 09:08:04 +0100

    add final imps

--- data/numeric imputations/imputation merge.R
+++ data/numeric imputations/imputation merge.R
@@ -4,7 +4,6 @@ n3 <- readRDS("data/numeric imputations/imp30-40k.rds") #30001-40000
 n4 <- readRDS("data/numeric imputations/imp40-50k.rds") # 40001 - 50000
 n5 <- readRDS("data/numeric imputations/imp50-70k.rds") #50001 - 70000
 n6 <- readRDS("data/numeric imputations/imp70-90k.rds") #700001 - 90000
-
 n7 <- readRDS("data/numeric imputations/imp90-110k.rds") #90001-11000<<<<
 n8 <- readRDS("data/numeric imputations/imp110-130k.rds") #110001 - 130000
 n9 <- readRDS("data/numeric imputations/imp130k+.rds") #130001 - 145231
