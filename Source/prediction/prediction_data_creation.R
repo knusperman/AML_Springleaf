c18b7f7bc6eb6da415655fc8ae9c28ac1a43f322
markusheuchert <m.heuchert@uni-muenster.de>
2017-01-05 14:00:06 +0100

    prediction data creation for new imp

--- Source/prediction/prediction_data_creation.R
+++ Source/prediction/prediction_data_creation.R
@@ -1,26 +1,31 @@
-
+####################################################################################
+#######OLD #########################################################################
 #numericalDataSample <- readRDS("data/numericalAttributes_cleansed_withoutFactors.rds")
 #numericalDataSample <- as.data.frame(numericalDataSample)
-data_numeric = as.data.frame(readRDS("data/final/numeric20k_predictionsample.rds"))
+#data_numeric1 = as.data.frame(readRDS("data/numeric imputations/imp10k.rds"))
+#data_numeric2 = as.data.frame(readRDS("data/numeric imputations/imp10-30k.rds"))
+#data_numeric3 = as.data.frame(readRDS("data/numeric imputations/imp30-40k.rds"))
+#data_numeric4 = as.data.frame(readRDS("data/numeric imputations/imp40-50k.rds"))
+#data_numeric5 = as.data.frame(readRDS("data/numeric imputations/imp50-70k.rds"))
+#data_numeric6 = as.data.frame(readRDS("data/numeric imputations/imp70-90k.rds"))
+#data_numeric7 = as.data.frame(readRDS("data/numeric imputations/imp90-110k.rds"))
+#data_numeric8 = as.data.frame(readRDS("data/numeric imputations/imp110-130k.rds"))
+#data_numeric9 = as.data.frame(readRDS("data/numeric imputations/imp130k+.rds"))
+#data_numeric = rbind(data_numeric1,data_numeric2,data_numeric3,data_numeric4,data_numeric5,data_numeric6,data_numeric7, data_numeric8,data_numeric9)
+#data_numeric = data_numeric[-c(130001),]
+####################################################################################
+####### END OLD ####################################################################
 
-data_numeric1 = as.data.frame(readRDS("data/numeric imputations/imp10k.rds"))
-data_numeric2 = as.data.frame(readRDS("data/numeric imputations/imp10-30k.rds"))
-data_numeric3 = as.data.frame(readRDS("data/numeric imputations/imp30-40k.rds"))
-data_numeric4 = as.data.frame(readRDS("data/numeric imputations/imp40-50k.rds"))
-data_numeric5 = as.data.frame(readRDS("data/numeric imputations/imp50-70k.rds"))
-data_numeric6 = as.data.frame(readRDS("data/numeric imputations/imp70-90k.rds"))
-data_numeric7 = as.data.frame(readRDS("data/numeric imputations/imp90-110k.rds"))
-data_numeric8 = as.data.frame(readRDS("data/numeric imputations/imp110-130k.rds"))
-data_numeric9 = as.data.frame(readRDS("data/numeric imputations/imp130k+.rds"))
+s <- readRDS("data/sample.rds") #1 = 1:50000, 2 = 500001:100000, 3 = 100001:145231 in train set
 
-data_numeric = rbind(data_numeric1,data_numeric2,data_numeric3,data_numeric4,data_numeric5,data_numeric6,data_numeric7, data_numeric8,data_numeric9)
-data_numeric = data_numeric[-c(130001),]
-remove(data_numeric1, data_numeric2,data_numeric3,data_numeric4,data_numeric5,data_numeric6,data_numeric7,data_numeric8,data_numeric9)
-
+data_numeric3a = as.data.frame(readRDS("data/numeric imputations/impsplit3_done1.rds"))
+data_numeric3b = as.data.frame(readRDS("data/numeric imputations/impsplit3_done2.rds"))
+data_numeric3 = rbind(data_numeric3a,data_numeric3b)
+data_numeric = data_numeric3 #for now
+remove(data_numeric3a,data_numeric3b,data_numeric3)
 data_factors = as.data.frame(readRDS("data/final/factorAttributes.rds"))[rownames(data_numeric),] #full train records. no NAs b/c treated as level
 data_strings = as.data.frame(readRDS("data/final/stringData_FINAL.rds"))[rownames(data_numeric),]
 data_dates   = as.data.frame(readRDS("data/final/dateData_FINAL.rds"))[rownames(data_numeric),] #f
-data_boolean = as.data.frame(readRDS("data/final/"))
 data_target  = as.data.frame(read.csv("data/target.csv"))[rownames(data_numeric),] #f #full train records
 
 
@@ -42,6 +47,6 @@ remove(data_factors)
 remove(data_strings)
 remove(data_dates)
 remove(data_target)
-
+mydata <- mydata[sample(1:20000,20000),which(colnames(mydata) %in% collist$cols_numeric)]
 #remove cols you do not want right now
 mydata <- mydata[,-which(colnames(mydata) %in% collist$cols_dates)]
