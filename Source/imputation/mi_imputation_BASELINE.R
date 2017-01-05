70ef1533a8240f6f6de5ddcf6c2dbfcf8d08a11d
Marcus Cramer <marcus_cramer@t-online.de>
2017-01-05 12:58:27 +0100

    parallel imputation

--- Source/imputation/mi_imputation_BASELINE.R
+++ Source/imputation/mi_imputation_BASELINE.R
@@ -60,13 +60,3 @@ for (i in 1:ncol(numericalData_imputed)) {
 }
 
 saveRDS(numericalData_imputed, "data/baseline_numericalAndFactor.rds")
-# numericalData_imputed = readRDS("data/baseline_numericalAndFactor.rds")
-
-# build entire baseline dataset
-trainData = readRDS("data/check1.rds")
-trainData = trainData[,-1]
-target = trainData[,ncol(trainData)]
-trainData = trainData[,-ncol(trainData)]
-dataset = as.data.frame(cbind(numericalData_imputed, 
-                        trainData[,!colnames(trainData) %in% colnames(numericalData_imputed)], target = target))
-saveRDS(dataset, "data/final/baselineDataset.rds")
