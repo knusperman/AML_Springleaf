e7bcb2e6f9278bcdd923259a64bbfc54b5100034
markusheuchert <m.heuchert@uni-muenster.de>
2017-01-06 23:55:24 +0100

    cleaning data creation

--- Source/prediction/prediction_data_creation.R
+++ Source/prediction/prediction_data_creation.R
@@ -29,24 +29,8 @@ library(doSNOW)
 library(randomForest)
 library(foreach)
 
-#data_numeric1a = as.data.frame(readRDS("data/numeric imputations/impsplit1_done1.rds"))
-#data_numeric1b = as.data.frame(readRDS("data/numeric imputations/impsplit1_done2.rds"))
-#data_numeric1 = rbind(data_numeric1a,data_numeric1b)
-#remove(data_numeric1a,data_numeric1b)
-#data_numeric2a = as.data.frame(readRDS("data/numeric imputations/impsplit2_done1.rds"))
-#data_numeric2b = as.data.frame(readRDS("data/numeric imputations/impsplit2_done2.rds"))
-#data_numeric2 = rbind(data_numeric2a,data_numeric2b)
-#remove(data_numeric2a,data_numeric2b)
-data_numeric3a = as.data.frame(readRDS("data/numeric imputations/impsplit3_done1.rds"))
-data_numeric3b = as.data.frame(readRDS("data/numeric imputations/impsplit3_done2.rds"))
-data_numeric3 = rbind(data_numeric3a,data_numeric3b)
-remove(data_numeric3a,data_numeric3b)
-#data_numeric = rbind(data_numeric1,data_numeric2,data_numeric3)
-data_numeric = data_numeric3 #imputed sample
-remove(data_numeric1,data_numeric2,data_numeric3)
-#check if rows correspond to sample, so that merge with factors etc. is correct:
-s <- readRDS("data/sample.rds") #1 = 1:50000, 2 = 500001:100000, 3 = 100001:145231 in train set
-#s[c(1:5,50001:50005,100001:100005)] == rownames(data_numeric)[c(1:5,50001:50005,100001:100005)]
+source("Source/prediction/data_functions.r")
+data_numeric = buildNumericData(c(3)) #imputed sample
 
 data_factors = as.data.frame(readRDS("data/final/factorAttributes.rds"))[rownames(data_numeric),] #full train records. no NAs b/c treated as level
 data_strings = as.data.frame(readRDS("data/final/stringData_FINAL.rds"))[rownames(data_numeric),]
@@ -54,11 +38,6 @@ data_dates   = as.data.frame(readRDS("data/final/dateData_FINAL.rds"))[rownames(
 data_target  = as.data.frame(read.csv("data/target.csv"))[rownames(data_numeric),] #f #full train records
 
 
-#collist = list("cols_numeric"=colnames(data_numeric),"cols_factors"=colnames(data_factors),"cols_strings" = colnames(data_strings), "cols_dates" = colnames(data_dates))
-#saveRDS(collist, "data/collist.rds")
-
-NAstatistics <- readRDS("data/NAstatistics.rds") #since data has no more information about amout of imputation in a column
-collist <- readRDS("data/collist.rds") #some data for selecting the right columns for predicting
 
 mydata <- cbind(data_numeric,data_factors,data_strings,data_dates, data_target)
 
