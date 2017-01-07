c0e9f2af2e9e98b692da1c75ba2b607e593409a7
markusheuchert <m.heuchert@uni-muenster.de>
2017-01-07 16:47:45 +0100

    rf tuning plan update

--- Source/prediction/rf_tuningplan.R
+++ Source/prediction/rf_tuningplan.R
@@ -5,30 +5,52 @@
 ntree = floor(ntree/parallel::detectCores())
 
 time = Sys.time()
-rf300_40 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=40))#todo by MC 
+rf300_40 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=40))#done by MH 0.7520
 diff = Sys.time() - time
 saveRDS(rf300_40, "data/prediction/rf300_40.rds")
 saveRDS(diff, "data/prediction/rf300_40_time.rds")
 
-
 time = Sys.time()
-rf300_50 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=50))#todo by MC 
+rf300_50 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=50))#done by MC 0.7520
 diff = Sys.time() - time
 saveRDS(rf300_50, "data/prediction/rf300_50.rds")
 saveRDS(diff, "data/prediction/rf300_50_time.rds")
 
-
-
 time = Sys.time()
-rf300_60 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=60))#todo by MC ::: best solution among the four
+rf300_60 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=60))#done by MH 0.7496
 diff = Sys.time() - time
 saveRDS(rf300_60, "data/prediction/rf300_60.rds")
 saveRDS(diff, "data/prediction/rf300_60_time.rds")
 
-
-
 time = Sys.time()
-rf_300_70 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=70))
+rf_300_70 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=70)) # done by MH 0.7518
 diff = Sys.time() - time
 saveRDS(rf300_70, "data/prediction/rf300_70.rds")
 saveRDS(diff, "data/prediction/rf300_70_time.rds")
+################################################################################
+################################################################################
+##### set mtry = 40. ############################################################
+################################################################################
+
+##### next tuning param: nodesize
+########## fixed: mtry = 40, ntree = 30
+
+#nodesize = 1 default, so we do not need to fit that
+
+rf_300_40_5= buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=70, nodesize=5)) # done by MH 0.7518
+diff = Sys.time() - time
+saveRDS(rf300_70, "data/prediction/rf300_70.rds")
+saveRDS(diff, "data/prediction/rf300_70_time.rds")
+
+rf_300_40_10 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=70,nodesize=10)) # done by MH 0.7518
+diff = Sys.time() - time
+saveRDS(rf300_70, "data/prediction/rf300_70.rds")
+saveRDS(diff, "data/prediction/rf300_70_time.rds")
+
+rf_300_40_20 = buildRF(mydata,classif.task,train.set,test.set,list(ntree=75,mtry=70,nodesize=20)) # done by MH 0.7518
+diff = Sys.time() - time
+saveRDS(rf300_70, "data/prediction/rf300_70.rds")
+saveRDS(diff, "data/prediction/rf300_70_time.rds")
+
+
+
--- Source/prediction/todo.r
+++ Source/prediction/todo.r
@@ -5,9 +5,18 @@ buildRPART(mydata,classif.task,train.set,test.set)->rpartdefault
 saveRDS(rpartdefault, "models/imputed/rpart_default.rds")
 remove(rpartdefault)
 buildRF(mydata,classif.task,train.set,test.set)->rfdefault
-saveRDS(rpartdefault, "models/imputed/rf_default.rds")
+saveRDS(rfdefault, "models/imputed/rf_default.rds")
 remove(rfdefault)
 
+####compare the default
+readRDS("models/imputed/xgboost_default.rds")->xgdefault
+readRDS("models/imputed/rpart_default.rds")->rpartdefault
+readRDS("models/imputed/rf_default.rds")->rfdefault
+png(filename = "fig/ROC_default.png",width = 1000,height = 800)
+getPlotAUC(list(XGBoost = xgdefault$predictions,RPART=rpartdefault$predictions, RF=rfdefault$predictions))
+dev.off()
+
+####
 rpart_tune = doParamOptimizationRPART(classif.task,minsplitvector = c(10,15,20,25,30,35),minbucketvector = c(10,15,20,25,30,35),cpvector = c(0.0001,0.0005,0.0010,0.0015))
 saveRDS(rpart_tune, "models/imputed/rpart_tune.rds")
 remove(rpart_tune)
