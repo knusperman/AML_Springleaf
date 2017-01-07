76e76c2b86c97d6c941a0d645244904187a6dbb2
marcuscramer91 <marcus_cramer@t-online.de>
2017-01-07 12:41:17 +0100

    asdf

--- Source/imputation/mi_imputation_helperFunctions.R
+++ Source/imputation/mi_imputation_helperFunctions.R
@@ -6,7 +6,7 @@ imputeWrapper = function(dataframe, colnum, miMatrix) {
   colname = colnames(dataframe)[colnum]
   impDF = dataframe[,c(colnum, impCols)]
   print(paste("imputing:", colname, "----",colnum, sep=" "))
-  if(sum(is.na(df_imputed[,colnum])) > 0) {
+  if(sum(is.na(dataframe[,colnum])) > 0) {
     result = complete(mi(impDF,n.chains=1, n.iter=15,parallel=FALSE),1)
     result = data.frame(row.names = row.names(impDF[,1]), res = result[unlist(result[paste("missing_",colname,sep="")]),1])
     gc() # collect some garbage
